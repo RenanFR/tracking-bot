797a959d1268bdb1c3caa270bc5d0b9cc8212c6c
Renan Rodrigues <renan.rodrigues.3@globalhitss.com.br>
2022-05-29 19:13:33 -0300

    first commit

---
+++ src/main/java/br/com/claro/discord/bot/TrackingBotApplication.java
@@ -0,0 +1,13 @@
+package br.com.claro.discord.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/claro/discord/bot/TrackingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.claro.discord.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
